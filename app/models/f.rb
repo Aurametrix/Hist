83744c2de507c75f1960da36a06e3ec90190dcca
Irene <irene@aurametrix.com>
2013-06-02 15:29:48 -0700

    category field instead of type  09032010-5

--- app/models/f.rb
+++ app/models/f.rb
@@ -3,7 +3,10 @@ class F
   field :name, :type => String
   field :type, :type => String
   field :description, :type => String
+  field :category, :type => String
 
-  scope :name_starts_with, lambda { |name| where(:name => /^#{name}/i) }
+  validates :name, :presence => true, :uniqueness => true
+  
+  scope :name_starts_with, lambda { |name| where(:name => /^#{name}/i).order_by(:name.desc) }
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,7 @@ $(function() {
 		source: function(request, response) {
                   $.getJSON('/search', {query: request.term}, function(data) {
         response($.map(data, function(item) {
-          return { label: item.type, value: item.name, parent: item.type, id: item._id}
+          return { label: item.type, value: item.name, category: item.category, id: item._id}
         }));
         
       });
@@ -23,7 +23,7 @@ $(function() {
 	.data( "autocomplete" )._renderItem = function( ul, item ) {
 		return $( "<li></li>" )
 			.data( "item.autocomplete", item )
-			.append( "<a> <span class='parent'>" + item.parent + ": </span><span class='value'>" + item.value + "</span></a>" )
+			.append( "<a> <span class='parent'>" + item.category + ": </span><span class='value'>" + item.value + "</span></a>" )
 			.appendTo( ul );
 	};
 });
