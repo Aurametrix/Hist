6ccb065b6ba7933d72c6144c478bb4cbec2f7be5
Irene <irene@aurametrix.com>
2013-06-06 15:52:00 -0700

    history with symptoms for users  09082010-15

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,8 +1,12 @@
 class History
   include Mongoid::Document
   include Mongoid::Timestamps
-  references_in :user
+  referenced_in :user
+  referenced_in :f
   
+  validates :f, :presence => true
+  validates :user, :presence => true
+
   field :note, :type => String
 
   scope :all_by_user, lambda { |user| { :where => { :user_id => user.id } } }
