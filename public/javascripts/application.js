6670c49b55b20ab1c45bcadad84912073c47a669
Irene <irene@aurametrix.com>
2013-06-05 20:27:56 -0700

    cleanup, removing unused blocks 09082010-8

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,8 @@
 $(function() {
-  $('form a.add_child').click(function() {
-    var assoc   = $(this).attr('data-association');
-    var content = $('#' + assoc + '_fields_template').html();
-    var regexp  = new RegExp('new_' + assoc, 'g');
-    var new_id  = new Date().getTime();
-
-$(function() {
+  window.setTimeout(function() {
+      $('#notices').slideUp('fast');
+  }, 2000);
+  
   $('form a.remove_child').live('click', function() {
     var hidden_field = $(this).prev('input[type=hidden]')[0];
     if(hidden_field) {
@@ -20,7 +17,7 @@ $(function() {
 	$('#query').autocomplete({
 		minLength: 1,
 		source: function(request, response) {
-                  $.getJSON('/search', {query: request.term}, function(data) {
+		  $.getJSON('/search', {query: request.term}, function(data) {
         response($.map(data, function(item) {
           return { label: item.type, value: item.name, category: item.category, id: item._id}
         }));
@@ -39,9 +36,11 @@ $(function() {
 		}
 	})
 	.data( "autocomplete" )._renderItem = function( ul, item ) {
+	  var cat = item.category ? item.category : item.value;
 		return $( "<li></li>" )
 			.data( "item.autocomplete", item )
-                        .append( "<a> <span class='parent'>" + cat + ": </span><span class='value'>" + item.value + "</span></a>" )
+			.append( "<a> <span class='parent'>" + cat + ": </span><span class='value'>" + item.value + "</span></a>" )
 			.appendTo( ul );
 	};
+  
 });
