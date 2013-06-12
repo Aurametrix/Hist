6ea0c0d30827480d56e52b296dda640936b16f5e
Irene <irene@aurametrix.com>
2013-06-12 14:32:27 -0700

    moving flash ajax logic to application controller filter 0911-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter :discard_flash_if_xhr
+  
+  protected
+    def discard_flash_if_xhr
+      flash.discard if request.xhr?
+    end
+    
+
 end
 
